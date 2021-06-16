a611de51147bd41791cea4d3f5048473e00553cb
Nastassia Kamiakova <nastassia294@gmail.com>
2021-06-16 14:05:36 +0300

    Extract function

--- test_runner.py
+++ test_runner.py
@@ -1,20 +1,11 @@
 def run_tests(module_list):
-    for function_name in find_test_functions(module_list):
+    for test_function in find_test_functions(module_list):
         try:
-            function_name()
-            print(f'\nTest function "{function_name.__name__}" run successfully!')
+            test_function()
+            test_result(test_function)
         except AssertionError as ex:
-            print(f'\nTest function "{function_name.__name__}" failed!', ex)
-            tb = ex.__traceback__
-            index = 0
-            while tb is not None:
-                if index > 0:
-                    print("---")
-                    print("\tfilename", tb.tb_frame.f_code.co_filename)
-                    print("\tname", tb.tb_frame.f_code.co_name)
-                    print("\tlineno", tb.tb_lineno)
-                tb = tb.tb_next
-                index += 1
+            test_result(test_function, ex)
+
 
 def find_test_functions(module_list):
     test_functions = []
@@ -25,3 +16,21 @@ def find_test_functions(module_list):
                 if callable(value):
                     test_functions.append(value)
     return test_functions
+
+
+def test_result(test_function, ex=None):
+    function_name = test_function.__name__
+    if ex is None:
+        print(f'\nTest function "{function_name}" run successfully!')
+    else:
+        print(f'\nTest function "{function_name}" failed!', ex)
+        tb = ex.__traceback__
+        index = 0
+        while tb is not None:
+            if index > 0:
+                print("---")
+                print("\tfilename", tb.tb_frame.f_code.co_filename)
+                print("\tname", tb.tb_frame.f_code.co_name)
+                print("\tlineno", tb.tb_lineno)
+            tb = tb.tb_next
+            index += 1
