afe9a13afcd63c8085078b316de04b6d263b6ea2
Nastassia Kamiakova <nastassia294@gmail.com>
2021-06-16 13:13:49 +0300

    move doc line

--- test_runner.py
+++ test_runner.py
@@ -1,12 +1,12 @@
 # find all test functions
 
 def run_test(module_list):
+    """Finds all functions which begin with `test_` and executes them."""
     functions_to_test = []
     for module in module_list:
         for function in dir(module):
             if function.startswith('test_'):
                 functions_to_test.append(getattr(module, function))
-    """Finds all functions which begin with `test_` and executes them."""
     for function in functions_to_test:
         if callable(function):
             try:
